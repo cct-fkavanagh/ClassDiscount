9cbd2025477a6582cd4a4949570821b5f31276c7
cct-fkavanagh <sba24298@student.cct.ie>
2024-10-25 13:40:53 +0100

    trying switch and if/else
    Unfortunately never had time to go into more detail over these two. Testing both to see which works best, likely will end up sticking with if/else as I want custClass AND yearCust to match

--- src/classdiscount/ClassDiscount.java
+++ src/classdiscount/ClassDiscount.java
@@ -3,13 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package classdiscount;
-
+import java.time.LocalDate; // added to set current date beforehand
 /**
  *
  * @author Francis
  */
 public class ClassDiscount {
 
+    public static double calculateDiscountedPrice(int custClass, int yearCust, double valPrice) {
+        
+        double valDiscount = valPrice;
+        int currYear = LocalDate.now().getYear();
+        
+        switch(custClass){
+                case 1:
+                    if(yearCust < currYear - 5){
+                        valDiscount = valPrice  * 0.8;
+                        System.out.println("Total after 20% discount: " + valDiscount);
+                    }
+                    }
+    }
     /**
      * @param args the command line arguments
      */
