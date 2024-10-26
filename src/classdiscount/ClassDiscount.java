79544312e9218b7072d0f3d50d3782ad59e410d2
cct-fkavanagh <sba24298@student.cct.ie>
2024-10-26 20:48:39 +0100

    Added error and write to statement
    Added a parameter stating if there's an issue with a customer's class and stating what their class is written as. Also added code to create a new customerdiscount.txt file with the goal of writing custName ad valDiscount for all customers

--- src/classdiscount/ClassDiscount.java
+++ src/classdiscount/ClassDiscount.java
@@ -4,8 +4,11 @@
  */
 package classdiscount;
 import java.time.LocalDate; // added to set current date beforehand
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,34 +26,38 @@ public class ClassDiscount {
         
         if (custClass == 1 && yearCust == currYear) {
             valDiscount = valPrice * 0.7;
-            System.out.println("Applied 30% Discount");
+            System.out.println("Applied 30% Discount:");
             
         } else if(custClass == 1 && yearCust < currYear) { //can i add a loop to make the last 5 years more accurate?
             valDiscount = valPrice * 0.8;
-            System.out.println("Applied 20% Discount");
+            System.out.println("Applied 20% Discount:");
             
         } else if(custClass == 1 && yearCust < currYear - 5) {
             valDiscount = valPrice * 0.9;
-            System.out.println("Applied 10% Discount");
+            System.out.println("Applied 10% Discount:");
             
         } else if (custClass == 2 && yearCust == currYear ) {
             valDiscount = valPrice * 0.85;
-            System.out.println("Applied 15% Discount");
+            System.out.println("Applied 15% Discount:");
             
         } else if (custClass == 2 && yearCust < currYear) { //can i add a loop to make the last 5 years more accurate?
             valDiscount = valPrice * 0.87;
-            System.out.println("Applied 13% Discount");
+            System.out.println("Applied 13% Discount:");
             
         } else if (custClass == 2 && yearCust < currYear - 5) {
             valDiscount = valPrice * 0.95;
-            System.out.println("Applied 5% Discount");
+            System.out.println("Applied 5% Discount:");
             
         } else if (custClass == 3 && yearCust == currYear) {
             valDiscount = valPrice * 0.97;
-            System.out.println("Applied 3% Discount");
+            System.out.println("Applied 3% Discount:");
             
-        } else if (custClass == 3 && yearCust < currYear - 5) {
-            System.out.println("No Discount Applied");
+        } else if (custClass == 3 && yearCust < currYear) {
+            System.out.println("No Discount Applied:");
+            return valPrice;
+                    
+        } else if (custClass < 1) { //displays error message if a valid customer class is not detected
+            System.out.println("ERROR DETECTED!! Invalid Customer Class. Customer Class = " + custClass + " - No Discount Applied");
             return valPrice;
         }
         
@@ -61,24 +68,31 @@ public class ClassDiscount {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        File file = new File("customers.txt");
-        Scanner scan = null;
+
+        File file = new File("customers.txt"); //designating what file I want read
+        Scanner scan = null; //scanner set up outside try/catch so it can be used later
         try {
             scan = new Scanner(file);
         } catch (FileNotFoundException ex) {
             Logger.getLogger(ClassDiscount.class.getName()).log(Level.SEVERE, null, ex);
-            
+                       
         } while(scan.hasNextLine()) {
             
-            String custName = scan.nextLine();
-            double valPrice = Double.parseDouble(scan.nextLine());
-            int custClass = Integer.parseInt(scan.nextLine());
-            int yearCust = Integer.parseInt(scan.nextLine());
+            String custName = scan.nextLine(); //scans for customer name
+            double valPrice = Double.parseDouble(scan.nextLine()); //scans for total customer is spending
+            int custClass = Integer.parseInt(scan.nextLine()); // scans for customer class
+            int yearCust = Integer.parseInt(scan.nextLine()); // scans for the last purchase year from customer
             
             double valDiscount = calculateDiscountedPrice(custName, custClass, yearCust, valPrice);
             
-            System.out.println("Customer: " + custName + ", Total: " + valDiscount);
+            try {
+                BufferedWriter out = new BufferedWriter(new FileWriter("customerdiscount.txt"));
+                out.write("Customer: " + custName + ", Total: " + valDiscount);
+                out.newLine();            
+                //System.out.println("Customer: " + custName + ", Total: " + valDiscount);
+            } catch (IOException ex) {
+                Logger.getLogger(ClassDiscount.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }    
 }
