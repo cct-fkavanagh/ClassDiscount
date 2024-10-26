184ad292bfdeec19198353c05ddb0be2301a5b0b
cptbuff <francisjkavanagh@gmail.com>
2024-10-26 01:20:10 +0100

    Cleaned up main file + added txt file
    Removed repetitive return statements & added it at the end and added "return valPrice" when no discount is to be applied. Added customer.txt to project folder to start reading file. No more errors persist for now

--- src/classdiscount/ClassDiscount.java
+++ src/classdiscount/ClassDiscount.java
@@ -4,6 +4,8 @@
  */
 package classdiscount;
 import java.time.LocalDate; // added to set current date beforehand
+import java.io.File;
+
 /**
  *
  * @author Francis
@@ -18,50 +20,45 @@ public class ClassDiscount {
         if (custClass == 1 && yearCust == currYear) {
             valDiscount = valPrice * 0.7;
             System.out.println("Applied 30% Discount");
-            return valDiscount;
             
         } else if(custClass == 1 && yearCust < currYear) { //can i add a loop to make the last 5 years more accurate?
             valDiscount = valPrice * 0.8;
             System.out.println("Applied 20% Discount");
-            return valDiscount;
             
         } else if(custClass == 1 && yearCust < currYear - 5) {
             valDiscount = valPrice * 0.9;
             System.out.println("Applied 10% Discount");
-            return valDiscount;
             
         } else if (custClass == 2 && yearCust == currYear ) {
             valDiscount = valPrice * 0.85;
             System.out.println("Applied 15% Discount");
-            return valDiscount;
             
         } else if (custClass == 2 && yearCust < currYear) { //can i add a loop to make the last 5 years more accurate?
             valDiscount = valPrice * 0.87;
             System.out.println("Applied 13% Discount");
-            return valDiscount;
             
         } else if (custClass == 2 && yearCust < currYear - 5) {
             valDiscount = valPrice * 0.95;
             System.out.println("Applied 5% Discount");
-            return valDiscount;
             
         } else if (custClass == 3 && yearCust == currYear) {
             valDiscount = valPrice * 0.97;
             System.out.println("Applied 3% Discount");
-            return valDiscount;
+            
         } else if (custClass == 3 && yearCust < currYear - 5) {
             System.out.println("No Discount Applied");
             return valPrice;
         }
+        
+        return valDiscount;
     }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int currYear = LocalDate.now().getYear();
-        LoopClass loop = new LoopClass();
     
     }
     
