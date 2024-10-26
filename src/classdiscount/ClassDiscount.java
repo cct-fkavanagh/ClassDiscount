4731398bd3507446464d67064d0b9f5de8797bb1
cct-fkavanagh <sba24298@student.cct.ie>
2024-10-26 21:16:22 +0100

    Added github link
    Added link to github repo on ln 1

--- src/classdiscount/ClassDiscount.java
+++ src/classdiscount/ClassDiscount.java
@@ -1,7 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+// github link: https://github.com/cct-fkavanagh/ClassDiscount
+
 package classdiscount;
 import java.time.LocalDate; // added to set current date beforehand
 import java.io.BufferedWriter;
